ff2de6b9bbe5bad7b49ed1204a585e0870b45827
MbeckerHTW <Marco@10.9.40.35>
2018-03-07 12:23:32 +0100

    KontaktServiceImpl definiert der funktionen

--- Server/src/main/java/de/htwsaar/server/service/KontaktServiceImpl.java
+++ Server/src/main/java/de/htwsaar/server/service/KontaktServiceImpl.java
@@ -5,6 +5,37 @@ public class KontaktServiceImpl {
 
 	public void handleKontaktConfig(Message message)
 	{
+		switch (message.getAktion()) {
+		case Actions.Kontakt_Hinzufuegen:
+						
+			kontaktHinzufuegen(message);
+			break;
+			
+		case Actions.Kontakt_Loeschen:
+			kontaktLoeschen(message);
+			break;
+			
+		case Actions.Kontakt_Blockieren:
+			kontaktBlockieren(message);
+			break;
+			
+		
+
+		}
+	}
+	
+	private void kontaktHinzufuegen(Message message)
+	{
+		
+	}
+	
+	private void kontaktLoeschen(Message message)
+	{
+		
+	}
+	
+	private void kontaktBlockieren(Message message)
+	{
 		
 	}
 }
