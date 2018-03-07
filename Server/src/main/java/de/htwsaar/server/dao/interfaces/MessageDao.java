a2a71df0fa67d10b1766999c54a114f630ac70cd
Marco <Marco@10.9.40.35>
2018-03-07 13:44:15 +0100

    MessageDao angepasst

--- Server/src/main/java/de/htwsaar/server/dao/interfaces/MessageDao.java
+++ Server/src/main/java/de/htwsaar/server/dao/interfaces/MessageDao.java
@@ -5,9 +5,6 @@ import de.htwsaar.server.dataclass.*;
 
 public interface MessageDao {
 	public void SaveMessage(Message message, User empfaenger);
-	public void kontaktHinzufügen(Message message);
-	public void kontaktLöschen(Message message);
-	public void kontaktBlockieren(Message message);
 	public List<Message> alleUngeleseneNachrichten(int timestamp);
 	public Message readMessage();
 
