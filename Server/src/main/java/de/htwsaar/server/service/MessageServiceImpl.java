94f75213647642dfb27897c3d1c7e398191ff46d
alnenno <alnenno@goat.si>
2018-03-12 00:16:40 +0100

    javadoc and minor code changes to MessageServiceImpl

--- Server/src/main/java/de/htwsaar/server/service/MessageServiceImpl.java
+++ Server/src/main/java/de/htwsaar/server/service/MessageServiceImpl.java
@@ -10,22 +10,31 @@ import java.util.List;
 import de.htwsaar.server.dataclass.*;
 import de.htwsaar.server.service.interfaces.MessageService;
 
-
+/**
+ * this class handles direct and group messages.
+ * handleMessage(Message message) should be called when a message is received from a client.
+ */
 public class MessageServiceImpl implements MessageService{
 	
 	UserDao userDao;
 	MessageDao messageDao;
 	GroupDao groupDao;
 
+	/**
+	 * default constructor
+	 * automatically starts a daemon thread in the background that interacts with the database and the network
+	 * do not create multiple instances of this class
+	 */
 	public MessageServiceImpl()
 	{
 		userDao = DaoObjectBuilder.getUserDao();
 		messageDao = DaoObjectBuilder.getMessageDao();
 		groupDao = DaoObjectBuilder.getGroupDao();
-		
+		startMessageServiceDaemon();
 	}
 
 	private Thread messageServiceDaemon;
+	
 	/**
 	 * Starts a Thread in the background.
 	 * This thread will periodically check for unsent messages in the database and send them if possible.
@@ -72,7 +81,7 @@ public class MessageServiceImpl implements MessageService{
 
 	/**
 	 * Handles the processing of a group message
-	 * @param message
+	 * @param message the message to send to a group
 	 */
 	private void gruppenNachrichten(Message message)
 	{
@@ -112,15 +121,15 @@ public class MessageServiceImpl implements MessageService{
 	
 	/**
 	 * sends a message as it was received to its recipient
-	 * @param message
-	 * @param User
+	 * @param message the message to send
+	 * @param empfaenger the recipient of the message
 	 */
-	private void sendeNachricht(Message message, User user){
+	private void sendeNachricht(Message message, User empfaenger){
 		//Schreibt die Nachricht in die Datenbank, mit dem Flag, dass die nachricht noch nicht zugestellt wurde.
-		messageDao.SaveMessage(message, user);
+		messageDao.SaveMessage(message, empfaenger);
 		message.setMessageID(messageDao.readMessageID());
 		
-		System.out.println("Absender: "+ message.getSender()+ " Empfäenger: "+ user.getAbsenderId()+ " IpAdresse: "+ user.getIpAdresse() + " MessageID: "+ message.getMessageID());
+		System.out.println("Absender: "+ message.getSender()+ " Empfäenger: "+ empfaenger.getAbsenderId()+ " IpAdresse: "+ empfaenger.getIpAdresse() + " MessageID: "+ message.getMessageID());
 		
 		
 		//TODO: bei erfolgreichem Zustellen, muss das FLAG Zugestellt auf 1 gesetzt werden.
