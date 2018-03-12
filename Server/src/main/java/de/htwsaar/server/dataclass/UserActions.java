14352735f48ab2f0e334072d0aa7343d1ce1ea12
MbeckerHtw <marco.bexi.becker@gmail.com>
2018-03-12 10:06:26 +0100

    Add files via upload

---
+++ Server/src/main/java/de/htwsaar/server/dataclass/UserActions.java
@@ -0,0 +1,13 @@
+package de.htwsaar.server.dataclass;
+
+/**
+ * Class for alle UserActions
+ * @author Marco
+ *
+ */
+public class UserActions {
+
+		public static final int USER_LOGIN = 0;
+		public static final int USER_AUTHENTIFIZIERUNG = 1;
+		public static final int USER_LOGOUT = 2;
+}
