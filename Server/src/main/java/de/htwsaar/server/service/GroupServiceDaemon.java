42d0e06f1434fd31d031b79f84beb9de2b2a9fdc
rtrautz <robin.trautz@gmail.com>
2018-03-15 17:42:24 +0100

    Threading implementiert

--- Server/src/main/java/de/htwsaar/server/service/GroupServiceDaemon.java
+++ Server/src/main/java/de/htwsaar/server/service/GroupServiceDaemon.java
@@ -1,5 +1,6 @@
 package de.htwsaar.server.service;
 
+import java.util.Iterator;
 import java.util.List;
 
 import de.htwsaar.server.dao.DaoObjectBuilder;
@@ -49,6 +50,48 @@ public class GroupServiceDaemon {
 	public void sendAllGroupInformation(List<User> user)
 	{
   
+	}
+	
+	
+	
+	private void startGroupServiceDaemon(){
+		Thread groupServiceDaemon= new Thread(new Runnable() {
+			
+			public void run() {
+				//TODO: implement
+				//Datenbank abfragen nach nutzern die grade online sind
+				//für jeden von denen:
+				//sende ihnen die gesamten informationen aller gruppen zu in denen sie sind
+				//schlafe für ein paar sekunden, damit das alles nicht zu oft gesendet wird
+				while (true)
+				{
+				List<User> user = daemon.getAllOnlineUser();
+				Iterator<User> i = user.iterator();
+				
+				while(i.hasNext()==true)
+				{
+					final User nextUser;
+					nextUser = i.next();
+					String userID = nextUser.getAbsenderId();
+					 List<Group> groupList = daemon.getGroupListForUser(userID);
+					 Iterator<Group> j = groupList.iterator();
+					 while(i.hasNext()==true)
+					 {
+						 nextGroup = j.next();
+						 int groupID = nextGroup.getGroupId();
+						 List<User> members = daemon.getMemberListOfGroup(groupID);
+					 }
+		         }
+				try {
+					Thread.sleep(6000);
+				} catch (InterruptedException e) {
+					
+				}
+				
+			   }
+		}
+		});
+		groupServiceDaemon.start();
 	}
 	
 	
