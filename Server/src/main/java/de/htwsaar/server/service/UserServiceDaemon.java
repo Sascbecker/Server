5241644c1b4505e2da5d3d7ad82f68252d6b6fc9
rtrautz <robin.trautz@gmail.com>
2018-03-15 17:14:04 +0100

    Threading implementiert

--- Server/src/main/java/de/htwsaar/server/service/UserServiceDaemon.java
+++ Server/src/main/java/de/htwsaar/server/service/UserServiceDaemon.java
@@ -1,5 +1,6 @@
 package de.htwsaar.server.service;
 
+import java.util.Iterator;
 import java.util.List;
 
 import de.htwsaar.server.dao.DaoObjectBuilder;
@@ -7,49 +8,85 @@ import de.htwsaar.server.dao.interfaces.UserDao;
 import de.htwsaar.server.dataclass.User;
 import de.htwsaar.server.service.interfaces.UserService;
 
-public class UserServiceDaemon {
+public class UserServiceDaemon extends Thread {
 
 	UserDao userDao;
 	UserService userService;
 
-
-	public UserServiceDaemon()
-	{
+	public UserServiceDaemon() {
 		userDao = DaoObjectBuilder.getUserDao();
 		userService = ServiceObjektBuilder.getUserService();
+		this.startUserServiceDaemon();
 	}
-	
-	
+
 	/**
 	 * Return a list of Users, which are Online
+	 * 
 	 * @return List
 	 */
-	
-	public List<User> getAllOnlineUser()
-	{
+
+	public List<User> getAllOnlineUser() {
 		List<User> user = userDao.getAllOnlineUser();
-		
+
 		return user;
 	}
-	
+
 	/**
-	 * sends a Request to each User in the List
-	 * if the Client isn�t sending a Reply before timeout, the user gets logged out
+	 * sends a Request to each User in the List if the Client isn�t sending a
+	 * Reply before timeout, the user gets logged out
 	 */
-	
-	public boolean ping(User user)
-	{
+
+	public boolean ping(User user) {
 		boolean online;
 		String ip = user.getIpAdresse();
 		online = de.htwsaar.service.serverConnector.ClientConnector.ping(ip);
 		return online;
 	}
+
 	/**
 	 * logs the User out
 	 */
-	public void logout(User user)
-	{
+	public void logout(User user) {
 		userService.userAbmelden(user);
-		
+
 	}
+
+	private void startUserServiceDaemon() {
+		
+			Thread userServiceDaemon = new Thread(new Runnable() {
+				@Override
+				public void run() {
+					while (true)
+					{
+						List<User> user = getAllOnlineUser();
+						Iterator<User> i = user.iterator();
+						
+						while (i.hasNext() == true) 
+						{
+							final User nextUser;
+							nextUser = i.next();
+							boolean online = ping(nextUser);
+							if (online == true) {
+
+							} else {
+								logout(nextUser);
+							}
+						}
+						try {
+							Thread.sleep(60000);
+						} catch (InterruptedException e) {
+							
+						}
+					}
+					
+
+				}
+
+			});
+			userServiceDaemon.start();
+		}
+	
+
+	
+	
 }
